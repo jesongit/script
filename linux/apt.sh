e847710726df2fcfd0448beda76c9b88d60cc7f2
Posase <188380780@qq.com>
2024-06-07 15:55:25 +0800

    Create apt.sh

---
+++ linux/apt.sh
@@ -0,0 +1,5 @@
+rm -f /etc/apt/sources.list.d/debian.sources
+echo "deb https://mirrors.aliyun.com/debian/ bookworm main non-free non-free-firmware contrib
+deb https://mirrors.aliyun.com/debian-security/ bookworm-security main
+deb https://mirrors.aliyun.com/debian/ bookworm-updates main non-free non-free-firmware contrib
+deb https://mirrors.aliyun.com/debian/ bookworm-backports main non-free non-free-firmware contrib" > /etc/apt/sources.list
