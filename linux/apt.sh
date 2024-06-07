81689708d85edee7097e46d23dda74cdfdb4a9c3
Posase <188380780@qq.com>
2024-06-07 16:01:49 +0800

    Update apt.sh

--- linux/apt.sh
+++ linux/apt.sh
@@ -1,3 +1,4 @@
+# curl -L https://raw.githubusercontent.com/jesongit/script/main/linux/apt.sh | bash
 rm -f /etc/apt/sources.list.d/debian.sources
 echo "deb https://mirrors.aliyun.com/debian/ bookworm main non-free non-free-firmware contrib
 deb https://mirrors.aliyun.com/debian-security/ bookworm-security main
